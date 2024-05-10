cc9208b0183f35f626fb5f7ba26b350c03b18903
br42 <60104573+br42@users.noreply.github.com>
2024-05-10 11:06:02 -0300

    Modificado script de dividir datasets em scripts-bdp/dataset-split.sh para maior modularidade.

--- scripts-bdp/dataset-split.sh
+++ scripts-bdp/dataset-split.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/bash
 
 (
-  head -n 1 dataset-bert/train2024.csv | tee dataset-bert/train-sample50.csv dataset-bert/test-sample50.csv > /dev/null ;
-  shuf < dataset-bert/train2024.csv > dataset-bert/shuf-sample.csv ;
-  sed -i "/$(head -n 1 dataset-bert/train2024.csv )/d" dataset-bert/shuf-sample.csv ;
-  dataset_size="$(wc -l < dataset-bert/shuf-sample.csv)" ;
-  head -n "+$((${dataset_size}*50/100))" dataset-bert/shuf-sample.csv >> dataset-bert/train-sample50.csv ;
-  tail -n "-$((${dataset_size}*50/100))" dataset-bert/shuf-sample.csv >> dataset-bert/test-sample50.csv ;
+  dataset_file="train-expanded.csv"
+  dataset_dir="dataset-bert/"
+  temp_sample="shuf-sample.csv"
+  train_sample="train-sample.csv"
+  test_sample="test-sample.csv"
+  proportion="50/100"
+  
+  head -n 1 "${dataset_dir}${dataset_file}" | tee "${dataset_dir}${train_sample}" "${dataset_dir}${test_sample}" > /dev/null ;
+  shuf < "${dataset_dir}${dataset_file}" > "${dataset_dir}${temp_sample}" ;
+  sed -i "/$(head -n 1 "${dataset_dir}${dataset_file}")/d" "${dataset_dir}${temp_sample}" ;
+  dataset_size="$(wc -l < "${dataset_dir}${temp_sample}")" ;
+  head -n "+$(("${dataset_size}"*"${proportion}"))" "${dataset_dir}${temp_sample}" >> "${dataset_dir}${train_sample}" ;
+  tail -n "-$(("${dataset_size}"*"${proportion}"))" "${dataset_dir}${temp_sample}" >> "${dataset_dir}${test_sample}" ;
 )
